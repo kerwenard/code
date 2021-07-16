edc7d5db99552d232998e6ce720faa9c9c4e068d
茶居 <1196395297@qq.com>
2021-07-16 17:25:50 +0800

    Update docs

--- code/other/build.sh
+++ code/other/build.sh
@@ -1,6 +1,7 @@
 timedatectl set-timezone Asia/Shanghai
 
 yum install git
+bash Anaconda3-2021.05-Linux-x86_64.sh
 pip3 install --upgrade pip
 pip3 install scrapy
 pip3 install scrapy-splash
@@ -10,7 +11,6 @@ pip3 install requests==2.20 -i http://pypi.douban.com/simple --trusted-host pypi
 pip3 install fake-useragent -i http://pypi.douban.com/simple --trusted-host pypi.douban.com
 pip3 install schedule -i http://pypi.douban.com/simple --trusted-host pypi.douban.com
 wget https://repo.anaconda.com/archive/Anaconda3-2021.05-Linux-x86_64.sh
-bash Anaconda3-2021.05-Linux-x86_64.sh
 firewall-cmd --zone=public --add-port=12347/tcp --permanent
 firewall-cmd --zone=public --add-port=9527/tcp --permanent
 ssh-keygen -t rsa
