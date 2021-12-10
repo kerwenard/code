824222e39542b5129a5c171123d0ace15fd326ea
茶居 <1196395297@qq.com>
2021-12-10 14:42:37 +0800

    ok

--- code/other/build.sh
+++ code/other/build.sh
@@ -1,7 +1,6 @@
 timedatectl set-timezone Asia/Shanghai
-
+yum -y install wget
 yum install git
-bash Anaconda3-2021.05-Linux-x86_64.sh
 pip3 install --upgrade pip
 pip3 install tmux -i http://pypi.douban.com/simple --trusted-host pypi.douban.com
 pip3 install scrapy -i http://pypi.douban.com/simple --trusted-host pypi.douban.com
@@ -16,6 +15,7 @@ pip3 install pytesseract  -i http://pypi.douban.com/simple --trusted-host pypi.d
 pip3 install pillow  -i http://pypi.douban.com/simple --trusted-host pypi.douban.com
 
 wget https://repo.anaconda.com/archive/Anaconda3-2021.05-Linux-x86_64.sh
+bash Anaconda3-2021.05-Linux-x86_64.sh
 firewall-cmd --zone=public --add-port=12347/tcp --permanent
 firewall-cmd --zone=public --add-port=9527/tcp --permanent
 ssh-keygen -t rsa
